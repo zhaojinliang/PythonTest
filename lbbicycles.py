d5d7a37691c98c53e9b0f02a8f680d7de7d8a41a
zhaojinliang <zhaojinliang606@gmail.com>
2022-02-21 00:06:14 +0800

    Python练习项目

---
+++ lbbicycles.py
@@ -0,0 +1,10 @@
+bicycles=["trek",'cannondale','redline','specialized']
+print(bicycles)
+print(bicycles[1])
+print(bicycles[1].title())
+print(bicycles[3])
+print(bicycles[-1])
+
+message="my first bycicles was a "+bicycles[1].title()+"!"
+print(message)
+print(len(bicycles))
