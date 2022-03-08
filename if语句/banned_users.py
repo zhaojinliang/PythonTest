6c6818a43f45bd290b96640f84cbfd888dad5a15
zhaojinliang <zhaojinliang606@gmail.com>
2022-03-09 01:33:51 +0800

    test

---
+++ if语句/banned_users.py
@@ -0,0 +1,4 @@
+banned_users = ['ander','sdsvf',' vsdsgfsf','sdfsdf']
+user = 'mmmmmm'
+if user not in banned_users:
+    print(f"{user.title()},you can post a resonse if you wish.")
---
+++ if语句/magic_number.py
@@ -0,0 +1,3 @@
+answer = 17
+if answer != 42:
+    print('Than is not the correct answer. Please try again!')
